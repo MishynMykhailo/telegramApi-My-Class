45040bd446a6fc781a2aa6938e6d97c84e5afda3
MishynMykhailo <misham18092001@gmail.com>
2024-01-12 13:02:22 +0200

    add: new functional for btn

--- class/TelegramBot.class.js
+++ class/TelegramBot.class.js
@@ -9,6 +9,7 @@ class TelegramBot {
     this.offset = 0; // Используется для отслеживания последнего обновления
     this.isFetchingUpdates = false;
     this.commandHandlers = {};
+    this.callbackHandlers = {};
   }
   // Made request with axios on get/post method
   request() {
@@ -53,28 +54,76 @@ class TelegramBot {
   // catch update
   async processUpdates(updates) {
     for (const update of updates) {
-      const { message } = update;
-      if (message) {
-        const chatId = message.chat.id;
-        const text = message.text;
+      const { message, callback_query } = update;
 
+      console.log("update", update);
+
+      // callback_query logics
+      if (callback_query) {
+        const { data, message } = callback_query;
+        if (this.callbackHandlers[data]) {
+          await this.answerCallbackQuery(callback_query.id);
+          await this.callbackHandlers[data](message);
+        }
+      }
+
+      // commands this text with "/"
+
+      if (message) {
+        const text = message.text;
         // Process commands
         if (text.startsWith("/")) {
           const command = text.slice(1); // удаляем "/"
-          console.log(this.commandHandlers);
           if (this.commandHandlers[command]) {
+            // Message я передаю как раз все сообщение, чтобы мог его использовать в методе как context
             await this.commandHandlers[command](message);
           }
         }
       }
     }
   }
-  // telegram method - sendMessage
-  async sendMessage(chatId, text, params = {}) {
+  // BUTTONS LOGICS
+
+  inlineKeyboard(buttons = [{ text, callback_data }]) {
+    const replyMarkup = {
+      inline_keyboard: [[...buttons]],
+    };
+    return replyMarkup;
+  }
+  ReplyKeyboardMarkup() {
+    return {};
+  }
+  // This method need for inline btns, that off active status on btn;
+  async answerCallbackQuery(callbackQueryId) {
     try {
-      const response = await this.request().get("sendMessage", {
-        chat_id: chatId,
-        text: text,
+      await this.request().post("answerCallbackQuery", {
+        callback_query_id: callbackQueryId,
+      });
+      console.log("Callback query answered:", callbackQueryId);
+    } catch (error) {
+      console.error("Error answering callback query:", error.message);
+    }
+  }
+  // bot.action("flip_a_coin", async (ctx) => {
+  //   await ctx.editMessageText(
+  //     `${getCoinSide()}\nОтредактировано: ${new Date().toISOString()}`,
+  //     coinInlineKeyboard
+  //   );
+  // });
+
+  action(text, handler) {
+    this.callbackHandlers[text] = handler;
+  }
+  // END BUTTON LOGICS
+
+  // TELEGRAM methods
+
+  // sendMessage
+  async sendMessage(chat_id, text, params = {}) {
+    try {
+      const response = await this.request().post("sendMessage", {
+        chat_id,
+        text,
         ...params,
       });
       console.log("Message sent:", response.data.result);
@@ -82,11 +131,12 @@ class TelegramBot {
       console.error("Error sending message:", error.message);
     }
   }
-
+  // Hears for command wtih "/"
   hears(command, handler) {
     this.commandHandlers[command] = handler;
   }
 
+  //  telegram methods END
   async launch() {
     console.log("Launching the bot...");
     // Periodically poll for updates (adjust the interval based on your needs)
--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 const TelegramBot = require("./class/TelegramBot.class.js");
 
 const bot = new TelegramBot(process.env.TOKEN);
+
 bot.hears("start", (context) => {
   bot.sendMessage(context.chat.id, "__hello__", {
-    reply_markup: {
-      keyboard: {
-        inline_keyboard: [
-          [{ text: "params", callback_data: "params" }],
-          [{ text: "params", callback_data: "params" }],
-        ],
-      },
-    },
+    reply_markup: bot.inlineKeyboard([
+      { text: "поздороваться", callback_data: "hi" },
+      { text: "попрощаться", callback_data: "bb" },
+    ]),
+    parse_mode: "MarkDownV2",
   });
 });
-
+bot.action("hi", (context) => {
+  // console.log("Action:hi", context.message.chat.id);
+  bot.sendMessage(context.chat.id, "Hello,my name Alfredo");
+});
+bot.action("bb", (context) => {
+  bot.sendMessage(context.chat.id, "Goodbay, have a good day");
+});
 bot.launch();
